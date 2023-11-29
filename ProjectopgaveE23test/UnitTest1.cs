4acfcf2b40792dbcb6df47816bb0121a6760a625
jaskdesktop <jak002@edu.zealand.dk>
2023-11-29 12:29:26 +0100

    Tested repository methods

--- ProjectopgaveE23test/UnitTest1.cs
+++ ProjectopgaveE23test/UnitTest1.cs
@@ -3,10 +3,10 @@ namespace ProjectopgaveE23test
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Assert.Fail();
-        }
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    Assert.Fail();
+        //}
     }
 }
--- ProjectopgaveE23test/UserRepositoryTest.cs
+++ ProjectopgaveE23test/UserRepositoryTest.cs
@@ -20,11 +20,11 @@ namespace ProjectopgaveE23test
         }
 
         [TestMethod]
-        public void TestAdd()
+        public void TestAddValid()
         {
             UserRepository repo = new UserRepository();
             User testUser = new User();
-            testUser.Username = "test";
+            testUser.Username = "uniquetest";
             testUser.Password = "test";
             testUser.Email = "test";
             testUser.Name = "test";
@@ -35,6 +35,137 @@ namespace ProjectopgaveE23test
             int countafter = repo.GetAllUsers().Count();
             Assert.AreEqual(countbefore+1, countafter);
         }
+        [TestMethod]
+        public void TestAddInvalid()
+        {
+            UserRepository repo = new UserRepository();
+            User testUser = new User();
+            testUser.Username = "test";
+            testUser.Password = "test";
+            testUser.Email = "test";
+            testUser.Name = "test";
+            testUser.Admin = true;
+            testUser.MembershipType = (MembershipType)3;
+            repo.AddUser(testUser);
+            int countbefore = repo.GetAllUsers().Count();
+            User badUser = new User();
+            badUser.Username = "test";
+            badUser.Password = "bad";
+            repo.AddUser(badUser);
+            int countafter = repo.GetAllUsers().Count();
+            Assert.AreEqual(countbefore, countafter);
+        }
 
+        [TestMethod]
+        public void TestRemoveValid()
+        {
+            UserRepository repo = new UserRepository();
+            User testUser = new User();
+            testUser.Username = "test";
+            testUser.Password = "test";
+            testUser.Email = "test";
+            testUser.Name = "test";
+            testUser.Admin = true;
+            testUser.MembershipType= (MembershipType)3;
+            repo.AddUser(testUser);
+            int countbefore = repo.GetAllUsers().Count();
+            repo.DeleteUser("test");
+            int countafter = repo.GetAllUsers().Count();
+            Assert.AreEqual(countbefore, countafter+1);
+        }
+
+        [TestMethod]
+        public void TestRemoveInvalid()
+        {
+            UserRepository repo = new UserRepository();
+            User testUser = new User();
+            testUser.Username = "test";
+            testUser.Password = "test";
+            testUser.Email = "test";
+            testUser.Name = "test";
+            testUser.Admin = true;
+            testUser.MembershipType = (MembershipType)3;
+            repo.AddUser(testUser);
+            int countbefore = repo.GetAllUsers().Count();
+            repo.DeleteUser("bad");
+            int countafter = repo.GetAllUsers().Count();
+            Assert.AreEqual(countbefore, countafter);
+        }
+
+        [TestMethod]
+        public void TestGetValid()
+        {
+            UserRepository repo = new UserRepository();
+            User testUser = new User();
+            testUser.Username = "test";
+            testUser.Password = "test";
+            testUser.Email = "test";
+            testUser.Name = "test";
+            testUser.Admin = true;
+            testUser.MembershipType = (MembershipType)3;
+            repo.AddUser(testUser);
+            User fetchedUser = repo.GetUser("test");
+            Assert.AreEqual(testUser, fetchedUser);
+        }
+        [TestMethod]
+        public void TestGetInvalid()
+        {
+            UserRepository repo = new UserRepository();
+            User testUser = new User();
+            testUser.Username = "test";
+            testUser.Password = "test";
+            testUser.Email = "test";
+            testUser.Name = "test";
+            testUser.Admin = true;
+            testUser.MembershipType = (MembershipType)3;
+            repo.AddUser(testUser);
+            User fetchedUser = repo.GetUser("bad");
+            Assert.AreNotEqual(testUser, fetchedUser);
+        }
+
+        [TestMethod]
+        public void TestUpdateValid()
+        {
+            UserRepository repo = new UserRepository();
+            User testUser = new User();
+            testUser.Username = "test";
+            testUser.Password = "test";
+            testUser.Email = "test";
+            testUser.Name = "test";
+            testUser.Admin = true;
+            testUser.MembershipType = (MembershipType)3;
+            repo.AddUser(testUser);
+            User newUser = new User();
+            newUser.Username = "test";
+            newUser.Password = "test";
+            newUser.Email = "newtest";
+            newUser.Name = "newtest";
+            newUser.Admin = true;
+            newUser.MembershipType =(MembershipType)2;
+            repo.UpdateUser(newUser);
+            Assert.AreEqual(repo.GetUser("test"), newUser);
+        }
+        [TestMethod]
+        public void TestUpdateInvalid()
+        {
+            UserRepository repo = new UserRepository();
+            User testUser = new User();
+            testUser.Username = "test";
+            testUser.Password = "test";
+            testUser.Email = "test";
+            testUser.Name = "test";
+            testUser.Admin = true;
+            testUser.MembershipType = (MembershipType)3;
+            repo.AddUser(testUser);
+            User newUser = new User();
+            newUser.Username = "bad";
+            newUser.Password = "test";
+            newUser.Email = "newtest";
+            newUser.Name = "newtest";
+            newUser.Admin = true;
+            newUser.MembershipType = (MembershipType)2;
+            repo.UpdateUser(newUser);
+            Assert.AreNotEqual(repo.GetUser("test"), newUser);
+        }
     }
 }
