336088dd08604b700928882e1afd2829819f29ec
Katrine <kah003@edu.zealand.dk>
2023-11-30 11:10:21 +0100

    tilføjet resten

--- ProjektopgaveE23/Pages/Events/EditEvent.cshtml.cs
+++ ProjektopgaveE23/Pages/Events/EditEvent.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjektopgaveE23.Interfaces;
+using ProjektopgaveE23.Models;
 
 namespace ProjektopgaveE23.Pages.Events
 {
     public class EditEventModel : PageModel
     {
-        public void OnGet()
+
+        private IEventRepository _repo;
+
+        [BindProperty]
+        public Event UpdatedEvent { get; set; }
+
+        public EditEventModel(IEventRepository eventRepository)
         {
+            _repo = eventRepository;
         }
+
+
+        public void OnGet(int id)
+        {
+            UpdatedEvent=_repo.GetEvent(id);
+        }
+
+        public IActionResult OnPostUpdate() 
+        {
+            _repo.UpdateEvent(UpdatedEvent);
+            return RedirectToPage("index");
+        }
+
+        public IActionResult OnPostCancel()
+        {
+            return RedirectToPage("index");
+        }
+
     }
 }
