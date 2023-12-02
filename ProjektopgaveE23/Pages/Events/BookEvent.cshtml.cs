f916f34ce828625a0faf42a7a3ad8bfcfb203699
Katrine <katrinemichala@gmail.com>
2023-12-02 23:18:50 +0100

    :)

--- ProjektopgaveE23/Pages/Events/BookEvent.cshtml.cs
+++ ProjektopgaveE23/Pages/Events/BookEvent.cshtml.cs
@@ -56,7 +56,7 @@ namespace ProjektopgaveE23.Pages.Events
             return RedirectToPage("Index");
         }
 
-        List<int> numbers = new List<int>() {1,2,3,4,5,6,7,8,9,10};
+        List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
         
     }
