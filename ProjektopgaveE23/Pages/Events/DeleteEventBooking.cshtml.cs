867c628b9014053c2255537b1b78aceddea58a51
Katrine <kah003@edu.zealand.dk>
2023-12-04 12:39:47 +0100

    Delete added

--- ProjektopgaveE23/Pages/Events/DeleteEventBooking.cshtml.cs
+++ ProjektopgaveE23/Pages/Events/DeleteEventBooking.cshtml.cs
@@ -1,15 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjektopgaveE23.Interfaces;
+using ProjektopgaveE23.Models;
 
 namespace ProjektopgaveE23.Pages.Events
 {
     public class DeleteEventBookingModel : PageModel
     {
-        
-        
-        
-        public void OnGet()
+        private IEventBookingRepo _eventBookingRepo;
+        private IEventRepository _eventRepository;
+
+        [BindProperty]
+        public EventBooking BookingToDelete { get; set; }
+
+        [BindProperty]
+        public Event Event {  get; set; }
+
+        public DeleteEventBookingModel(IEventBookingRepo eventBookingRepo, IEventRepository eventRepository)
         {
+            _eventBookingRepo = eventBookingRepo;
+            _eventRepository = eventRepository;
+
+        }
+
+
+        public void OnGet(int deleteId)
+        {
+            BookingToDelete = _eventBookingRepo.GetBooking(deleteId);
+            Event = _eventRepository.GetEvent(BookingToDelete.EventID);
+
+        }
+
+        public IActionResult OnPostDelete(int idNumber)
+        {
+            BookingToDelete = _eventBookingRepo.GetBooking(idNumber);
+            _eventBookingRepo.DeleteBooking(BookingToDelete);
+            return RedirectToPage("Index");
+
+        }
+
+        public IActionResult OnPostCancel() 
+        {
+            return RedirectToPage("Index");
         }
     }
 }
