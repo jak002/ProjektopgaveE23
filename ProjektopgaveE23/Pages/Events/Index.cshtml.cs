1ed701ed5d0a5615100513446db65402cb792713
Katrine <katrinemichala@gmail.com>
2023-12-05 12:55:52 +0100

    index start

--- ProjektopgaveE23/Pages/Events/Index.cshtml.cs
+++ ProjektopgaveE23/Pages/Events/Index.cshtml.cs
@@ -9,18 +9,27 @@ namespace ProjektopgaveE23.Pages.Events
     {
 
         private IEventRepository _repo;
+        private IUserRepository _userRepository;
 
         public List<Event> Events { get; set; }
 
-        public IndexModel(IEventRepository eventRepository)
+        public User CurrentUser { get; set; }
+
+        public IndexModel(IEventRepository eventRepository, IUserRepository userRepository)
         {
             _repo = eventRepository;
+            _userRepository = userRepository;
         }
 
 
 
         public void OnGet()
         {
+            string sessionusername = HttpContext.Session.GetString("Username");
+            if (sessionusername != null)
+            {
+                CurrentUser = _userRepository.GetUser(sessionusername);
+            }
             Events = _repo.GetAllEvents();
         }
     }
