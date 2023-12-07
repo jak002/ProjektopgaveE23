98569f8723db41e41439dfe971e6b231e5b4f5cf
jask <jask@itu.dk>
2023-12-07 13:29:08 +0100

    Validation

--- ProjektopgaveE23/Pages/Users/CreateUser.cshtml.cs
+++ ProjektopgaveE23/Pages/Users/CreateUser.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProjektopgaveE23.Interfaces;
 using ProjektopgaveE23.Models;
+using ProjektopgaveE23.Helpers;
 
 namespace ProjektopgaveE23.Pages.Users
 {
@@ -25,9 +26,14 @@ namespace ProjektopgaveE23.Pages.Users
         public IActionResult OnPost() 
         {
             bool valid = true;
-            if (NewUser.Email != null && !NewUser.Email.Contains("@") && !NewUser.Email.Contains("."))
+            if (!InputValidator.ValidateEmail(NewUser.Email))
             {
                 EmailMessage = "E-mail skal formateres korrekt";
+                valid = false;
+            }
+            if (!InputValidator.ValidatePhone(NewUser.PhoneNumber))
+            {
+                PhoneMessage = "Telefonnummer må kun indeholde tal, +, og mellemrum";
                 valid = false;
             }
 
