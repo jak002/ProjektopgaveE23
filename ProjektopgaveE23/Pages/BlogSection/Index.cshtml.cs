2c6a0a882078624ff1ca87b9cca2962be9982041
Katrine <kah003@edu.zealand.dk>
2023-12-08 12:54:35 +0100

    layout stuff

--- ProjektopgaveE23/Pages/BlogSection/Index.cshtml.cs
+++ ProjektopgaveE23/Pages/BlogSection/Index.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjektopgaveE23.Interfaces;
+using ProjektopgaveE23.Models;
 
 namespace ProjektopgaveE23.Pages.BlogSection
 {
     public class IndexModel : PageModel
     {
+        private IBlogRepository _blogRepository;
+
+        public List<Blog> Posts { get; set; }
+
+        public IndexModel(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+
         public void OnGet()
         {
+            Posts = _blogRepository.GetAllPosts();
         }
     }
 }
